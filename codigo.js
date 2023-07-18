9e7205c54797078322cd34ed950895512159dc95
giulianofertonani <giulianofertonani@gmail.com>
2023-07-18 16:31:59 -0300

    mi primer commit

---
+++ codigo.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let calcularMetBasal = document.getElementById('calcularMetBasal');
+
+calcularMetBasal.addEventListener('submit', calcMetBasal)
+
+function calcMetBasal(e){
+    e.preventDefault()
+ let peso = document.getElementById('peso').value;
+ let altura = document.getElementById('altura').value;
+ let edad = document.getElementById('edad').value;
+ let sexo = document.getElementById('sexo').value;
+
+if(sexo === 'men') {
+    let resultado= (10*peso) + (6.25*altura ) - (5*edad) + 5;
+    alert(`Tu metabolismo basal es de ${resultado}`);
+} else{
+    let resultado= (10*peso) + (6.25*altura ) - (5*edad) - 254;
+    alert(`Tu metabolismo basal es de ${resultado}`);
+}
+
+
+}
+
